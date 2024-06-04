1699b283203bb55f326530e74579490a62aeb958
darshit890 <88980439+darshit890@users.noreply.github.com>
2024-06-04 17:22:16 +0530

    modified:   app/(auth)/sign-up/[[...sign-up]]/page.tsx

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center mt-8">
           <ClerkLoaded>
-            <SignUp path="/sign-in" />
+            <SignUp path="/sign-up" />
           </ClerkLoaded>
           <ClerkLoading>
             <Loader2 className="animate-spin text-muted-foreground" />
